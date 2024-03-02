813052d08c35fdba17a8b44c10d2f1c662ce6402
erilovegrove <140354030+erilovegrove@users.noreply.github.com>
2024-03-03 01:38:15 +1300

    Maeda clock imitation

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## MDDN 242 Project 1: Time-based Media  
 
-Blah blah blah 
-rhi
+28/2/24
+I created a sketch of a window sill garden themed clock. On the left, a flower will grow a little bit every second, in the middle a watering can will fill up every minute, and on the right, a vine will bloom every hour. 
+
+1/2/24
+Maeda clock imitation: 
--- maeda_clock.js
+++ maeda_clock.js
@@ -2,12 +2,62 @@
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
   background(50); //  beige
-  fill(200); // dark grey
-  textSize(25);
+  fill(100); // dark grey
+  textSize(20);
   textAlign(CENTER, CENTER);
 
-  let textllinewidth1 = width/5
-  text("00\n01\n02\n03\n04\n05\n06\n07\n08\n09", textllinewidth1, height/2);
-  text("10\n11\n12\n13\n14\n15\n16\n17\n18\n19", textllinewidth1 + 50, height/2);
-  text("20\n21\n22\n23\n24\n25\n26\n27\n28\n29", textllinewidth1 + 100, height/2);
+translate (120, 1);
+rotate (PI/16);
+
+
+  let textllinewidth1 = width / 20;
+  let verticalspacing = 40;
+  let horizontalspacing = 40;
+  
+  // Draw the numbers from 00 to 59
+  for (let i = 0; i < 60; i++) {
+    let column = floor(i/10);
+    let row = i % 10;
+    let x = textllinewidth1 + column * horizontalspacing;
+    let y = verticalspacing * row + 20;
+    text(nf(i, 2), x, y); // nf() formats numbers to have two digits
+  }
+
+  // Emphasize the current second
+  
+  let column2 = floor(obj.seconds/10);
+  let row2 = obj.seconds % 10;
+  let currentSecondX = textllinewidth1 + column2 * horizontalspacing;
+  let currentSecondY = verticalspacing * row2 + 20;
+  push();
+  fill(255, 25, 0); // red
+  textSize (50);
+  text(nf(obj.seconds, 2), currentSecondX, currentSecondY );
+  pop();
+  
+// Emphasise current min
+
+  let columnmin = floor(obj.minutes/10);
+  let rowmin = obj.minutes % 10;
+  let currentMinuteX = textllinewidth1 + columnmin * horizontalspacing;
+  let currentMinuteY = verticalspacing * rowmin + 20;
+  push();
+  fill(300); // white
+  textSize (20);
+  text(nf(obj.minutes, 2), currentMinuteX, currentMinuteY );
+  pop();
+
+//emphasise current hour
+let columnHour = floor(obj.hours / 10);
+  let rowHour = obj.hours % 10;
+  let currentHourX = textllinewidth1 + columnHour * horizontalspacing;
+  let currentHourY = verticalspacing * rowHour + 20;
+  push();
+  fill(100); 
+  stroke (300);
+  strokeWeight (3);
+  textSize(20);
+  text(nf(obj.hours, 2), currentHourX, currentHourY);
+  pop();
+
 }
