362bf7a3cc576c86530e29bdf2cd1ca588ee0829
erilovegrove <140354030+erilovegrove@users.noreply.github.com>
2024-03-23 16:31:17 +1300

    finalise

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 I created a sketch of a window sill garden themed clock. On the left, a flower will grow a little bit every second, in the middle a watering can will fill up every minute, and on the right, a vine will bloom every hour. 
 ![Sketch 1](sketch2.jpg)
 
+Garden Clock by Eri Lovegrove
+
+influences
+design process 
+
 
 ### 2/3/24
 Maeda clock imitation: I really challenged myself to make this clock work, and used new functions that i've never used before to sort the numbers into rows and columns. 
@@ -21,3 +26,23 @@ I did some basic coding to make 12 circles around the bigger circle, where one w
 ### 12/3/24
 With some help, I have started to code numbers 0-59 that will keep track of the minutes. Nothing pretty at the moment, still getting a feel for the code. On the right, a dark green circle falls smoothly every second into a blue rectangle that increases every second. This will be the base for my watering can idea. 
 ![Preview 3](assets/preview3.jpg)
+
+
+
+Garden Clock by Eri Lovegrove
+Design Intentions
+The Garden Clock was created with the intention of being both functional and aesthetically pleasing. I aimed to design a clock that not only tells time but also serves as a visually captivating piece of decor. The key features include glowing flowers to indicate the hour, a central number display for minutes, and a watering can motif that fills up to represent the seconds.
+
+External Influence and Inspiration
+My main source of inspiration for this project came from a Pinterest board featuring garden-themed images (https://www.pinterest.nz/toothmushroom/garden/). The colors, vibes, and tones of these images influenced my design choices. I color-picked shades I liked from these images and incorporated similar floral designs into my clock.
+
+Design Process
+I began by familiarizing myself with coding techniques using the Maeda clock imitation as a reference point. This helped me get back into the coding flow and challenged me to utilize new functions.
+
+Starting with a sketch, I refined my design as I gained a better understanding of the assignment. Once comfortable, I translated my sketch into basic code foundations.
+
+Experimenting with Illustrator, I created visuals for my clock, focusing particularly on crafting the flowers. This part of the process was enjoyable as it allowed me to bring my design to life visually.
+
+Finally, I incorporated an alarm feature into the clock, which causes the flowers to blink when activated.
+
+This iterative process involved coding, sketching, visual design, and functionality testing, resulting in a clock that achieves both its functional and aesthetic goals.
--- clock.js
+++ clock.js
@@ -1,42 +1,4 @@
 
-
-// function draw_clock(obj) {
-//   background(237, 224, 197); // beige
-
-//   let centerX = width / 2;
-//   let centerY = height / 2;
-//   let bigRadius = 190; // Radius of the big circle
-//   let numCircles = 12; // Number of small circles
-//   let angleStep = TWO_PI / numCircles; // Angle step between each small circle
-
-//   // Draw the big circle
-//   fill(83, 110, 88); // Dark green
-//   noStroke();
-//   ellipse(centerX, centerY, bigRadius * 2);
-
-//   // Draw the small circles around the big circle
-//   let smallRadius = 10; // Radius of the small circles
-//   let distanceBetween = 30; // Distance between the big circle and small circles
-
-//   for (let i = 0; i < numCircles; i++) {
-//     push();
-//     translate(centerX, centerY); // Translate to the center of the canvas
-//     rotate(i * angleStep); // Rotate based on the angle
-//     translate(bigRadius + distanceBetween, 0); // Translate to the position outside the big circle
-    
-//     // Determine if the current circle should be highlighted
-//     if (i === obj.hours % numCircles) {
-//       fill(255, 0, 0); // Red color for highlighted circle
-//     } else {
-//       fill(200); // Gray color for other circles
-//     }
-    
-//     ellipse(0, 0, smallRadius * 2); // Draw the small circle
-//     pop(); // Restore the transformation state
-//   }
-// }
-
-
 // draw your own clock here based on the values of obj:
     //    obj.hours goes from 0-23
     //    obj.minutes goes from 0-59
@@ -51,18 +13,49 @@
 let raindrop; 
 let flower;
 let hourflowers = []
+let colourflower0;
+let colourflower1;
+let colourflower2;
+let colourflower3;
+let colourflower4;
+let colourflower5;
+let colourflower6;
+let colourflower7;
+let colourflower8;
+let colourflower9;
+let colourflower10;
+let colourflower11;
+let bigflower;
+let manyflowers;
+let watercan;
 let colourflowers = []
 
 function preload() {
     // Load the image before setup()
-    raindrop = loadImage('assets/raindrop.png'); // Replace with the actual path to your image file
+    raindrop = loadImage('assets/raindrop.png');  
     flower = loadImage('assets/flower.png')
     for (let i = 0; i < 6; i++) {
-      hourflowers[i] = loadImage('assets/flower' + i + '.png'); // Assuming your images are named as flower0.png, flower1.png, etc.
+      hourflowers[i] = loadImage('assets/flower' + i + '.png'); 
     }
     for (let i = 0; i < 6; i++) {
-      colourflowers[i] = loadImage ('assets/colour' + i + '.png');
+      colourflowers[i] = loadImage('assets/colour' + i + '.png'); 
     }
+
+    colourflower0 = loadImage('assets/colour0.png');
+    colourflower1 = loadImage('assets/colour1.png');
+    colourflower2 = loadImage('assets/colour2.png');
+    colourflower3 = loadImage('assets/colour3.png');
+    colourflower4 = loadImage('assets/colour4.png');
+    colourflower5 = loadImage('assets/colour5.png');
+    colourflower6 = loadImage('assets/colour6.png');
+    colourflower7 = loadImage('assets/colour7.png');
+    colourflower8 = loadImage('assets/colour8.png');
+    colourflower9 = loadImage('assets/colour9.png');
+    colourflower10 = loadImage('assets/colour10.png');
+    colourflower11 = loadImage('assets/colour11.png');
+    bigflower = loadImage ('assets/bigflower.png')
+    manyflowers = loadImage ('assets/manyflowers.png')
+    watercan = loadImage ('assets/watercan.png')
   }
 
 function draw_clock(obj) {
@@ -75,122 +68,132 @@ function draw_clock(obj) {
   imageMode (CENTER) ;
   
   fill(97, 163, 126); 
-  ellipse (0, 0, 350) //center
+  ellipse (0, 0, 350) 
 
-
-  
 
    for (let i = 0; i < 6; i++) {
     push();
     rotate(360 / 12 * i);
+
     image(hourflowers[i], 0, -215, 35, 35);
-    image(hourflowers[i], 0, 215, 35, 35); // Adjust position and size as needed
+    image(hourflowers[i], 0, 215, 35, 35); 
     pop();
   }
-
-
-  // for(let i=0; i < 12; i ++){
-  //   rotate (360/12)
-  //   fill (56, 84, 53);
-  //   ellipse (0, -210, 15)
-  // }
  
- let hourHand;
   if (obj.hours < 12) {
-    
     hourHand = map(obj.hours, 0, 11, 0, 330); //330 is 11:00
   } else {
     hourHand = map(obj.hours, 12, 23, 0, 330); 
   }
 
-  
 
-  for (let i = 0; i < 6; i ++) {
-  
+  push();
   rotate(hourHand);
-    if (obj.hours === i) {
-    
-  // fill (81, 207, 200);
-  // ellipse(0, -210, 15);
-  rotate(360 / 12 * i);
-  image (colourflowers[i],0, -215, 35, 35 );
-  image(colourflowers[i], 0, 215, 35, 35);
-  }
+  const specificHours1 = [0, 12];
+  const specificHours2 = [1, 13]
+  const specificHours3 = [2, 14]
+  const specificHours4 = [3, 15];
+  const specificHours5 = [4, 16];
+  const specificHours6 = [5, 17];
+  const specificHours7 = [6, 18];
+  const specificHours8 = [7, 19];
+  const specificHours9 = [8, 20];
+  const specificHours10 = [9, 21];
+  const specificHours11 = [10, 22];
+  const specificHours12 = [11, 23];
 
-  
-}
+   if (specificHours1.includes(obj.hours)) {
+    image (colourflower0, 0, -215, 55, 55);
+   }
+   if (specificHours2.includes(obj.hours)) {
+    image (colourflower1, 0, -215, 55, 55);
+   }
+   if (specificHours3.includes(obj.hours)) {
+    image (colourflower2, 0, -215, 55, 55);
+   }
+   if (specificHours4.includes(obj.hours)) {
+    image (colourflower3, 0, -215, 55, 55);
+   }
+   if (specificHours5.includes(obj.hours)) {
+    image (colourflower4, 0, -215, 55, 55);
+   }
+   if (specificHours6.includes(obj.hours)) {
+    image (colourflower5, 0, -215, 55, 55);
+   }
+   if (specificHours7.includes(obj.hours)) {
+    image (colourflower6, 0, -215, 55, 55);
+   }
+   if (specificHours8.includes(obj.hours)) {
+    image (colourflower7, 0, -215, 55, 55);
+   }
+   if (specificHours9.includes(obj.hours)) {
+    image (colourflower8, 0, -215, 55, 55);
+   }
+   if (specificHours10.includes(obj.hours)) {
+    image (colourflower9, 0, -215, 55, 55);
+   }
+   if (specificHours11.includes(obj.hours)) {
+    image (colourflower10, 0, -215, 55, 55);
+   }
+   if (specificHours12.includes(obj.hours)) {
+    image (colourflower11, 0, -215, 55, 55);
+   }
 
-  //minutes
+
+  pop();
 
 
-
-  push ()
-  let flowerXpos = [0,40,90, 0, -50, -90, -30]
-  let flowerYpos = [0,40,-90,100, -50, 90, -30]
-  // this for loop draw the flowers
-
-
-  for(let i = 0; i <= 7; i++  ){
-    if (flower) {
-      image(flower, flowerXpos[i] - 20, flowerYpos[i] - 25, 50, 50); // Adjust position and size as needed
-    }
-  
-    text(i,flowerXpos[i], flowerYpos[i]);
-  }
-
-
-for (let i=0; i<=6; i++) {
-  if (obj.minutes == i) {
-    fill (255);
-    text(i,flowerXpos[i], flowerYpos[i]);
-  }
-}
-  pop()
+//minutes
+image (bigflower, 0, 0, 240, 240);
+image (manyflowers, 0, 0, 330, 330);
 
 
   push()
   fill (255);
-  text ((obj.minutes), -20, -20);
+  textSize (60);
+  textAlign (CENTER);
+  text ((obj.minutes), 0, 20);
   pop()
 
-
-
   //seconds
-  let waterY = map (obj.millis, 0, 999, -100, 150);
+  
+  let waterY = map (obj.millis, 0, 999, -300, 220);
   if (raindrop) {
-    image(raindrop, 280, waterY, 20, 20); // Adjust position and size as needed
+    image(raindrop, 290, waterY, 20, 30); 
   }
 
 push ()
 fill (158, 218, 230);
 rectMode (CORNERS);
-let waterfill = map (obj.seconds, 0, 59, 180, 70);
-rect(250, waterfill, 350, 180);
+let waterfill = map (obj.seconds, 0, 59, 225, 150);
+rect(250, waterfill, 340, 225);
 
-  // rect (250, 50, waterfill);
-  // let waterfill = map (obj.seconds, 0, 59, 0, 100);
+  image (watercan, 280, 180, 200, 100);
   pop()
+//alarm
 
-  
+
+
+  if(obj.seconds_until_alarm < 0 || obj.seconds_until_alarm == undefined) {
+  }
+    else if (obj.seconds_until_alarm > 0){
+} else {
+   
+ for (let i = 0; i < 6; i++) {
+   push();
+    if (obj.seconds % 2 == 0) {
+      rotate(360 / 12 * i);
+    
+    image(colourflowers[i], 0, -215, 55, 55);
+    image(colourflowers[i], 0, 215, 55, 55); 
+    }
+      
+    pop();
+     }
+ 
+}
+
 
 }
 
 
-
-//opacity cirles
-
- // let OpacityAm = 35;
-
-  // let blue = color (140, 255, 251, OpacityAm);
-  // let purple = color (175, 133, 255, OpacityAm);
-
-  // let minForLerp = map ( obj.minutes, 0, 59, 0, 1);
-  // let colorOfOrb = lerpColor (blue, purple, minForLerp);
-
-  // let sizeStep = 15;
-  // let howManyCircles = map (obj.seconds, 0, 59, 20, 40);
-
-  // fill (colorOfOrb);
-  // for (let i = 0; i < howManyCircles; i++) {
-  //   ellipse (0,0, sizeStep*i);
-  // }
